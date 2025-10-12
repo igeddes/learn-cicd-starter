a98678400db110265fcd03b1a851a20c276aecf1
igeddes <iaingeddes@gmail.com>
2025-10-12 19:19:29 +0100

    Added linter

--- main.go
+++ main.go
@@ -24,6 +24,10 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
+func unused() string {
+	return "Barney is a dinosaur"
+}
+
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
